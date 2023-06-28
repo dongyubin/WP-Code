5ae1553e631097ce7b579cad0d179c1e36d70bd8
dongyubin <892457803@qq.com>
2023-06-28 10:02:00 +0800

    新增：关于本站

---
+++ 关于本站/page-about.php
@@ -0,0 +1,425 @@
+<?php
+/*
+Template Name: 关于页面
+*/
+get_header();?>
+<div class="site-content container">
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="content-area">
+				<main class="site-main">
+				<article class="type-post post">
+				<header class="entry-header page">
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+				</header>
+				<div class="entry-wrapper">
+					<div class="entry-content u-clearfix">
+					     <div class="about-page">
+    <div class="author-box">
+      <img alt="" src="https://gravatar.loli.net/avatar/3f86f3f1aa105924d030b7d3040a0037?s=200&amp;d=wavatar&amp;r=g"
+        class="avatar avatar-200 photo">
+    </div>
+    <div class="author-content">
+      <div class="author-content-item author-content-left">
+        <div class="item-titles">
+          <div class="item-title">欢迎来自 <span id="address"></span> 的朋友，到访文武科技柜👋</div>
+          <div class="item-title2">我是 斌仔</div>
+          <div class="item-title">是一名 博主、打工人、瞎折腾星人（软件、NAS）</div>
+        </div>
+        <div class="item-link">
+          <a href="http://wpa.qq.com/msgrd?v=3&uin=892457803&site=qq&menu=yes"><img src="https://cdn.wangdu.site/WP/images/about-QQ.png" /></a>
+          <a href="https://space.bilibili.com/302553824"><img src="https://cdn.wangdu.site/WP/images/about-bilibili.png" /></a>
+          <a href="https://img.wangdu.site/2023/202304101630785.gif"><img src="https://cdn.wangdu.site/WP/images/about-wechat.png" /></a>
+        </div>
+      </div>
+      <div class="author-content-item author-content-right">
+        <div>座右铭</div>
+        <div class="item-motto">知足常乐</div>
+      </div>
+    </div>
+    <div class="author-content">
+      <div class="author-content-item author-movies">
+        <div class="item-movies">
+          <div class="movie-category">爱好电影类型</div>
+          <span>真实事件改编</span>
+          <div class="movie-button">
+            <div>
+              <a href="https://www.wangdu.site/douban">
+                <span>我的观影</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="author-content-item author-movies">
+        <div class="item-movies item-good">
+          <div class="movie-category">好物分享</div>
+          <span>键鼠、NAS</span>
+          <div class="movie-button">
+            <div>
+              <a href="https://www.wangdu.site/equipment">
+                <span>我的好物</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="author-content-item author-movies">
+        <div class="item-movies item-music">
+          <div class="movie-category">爱好音乐类型</div>
+          <span>好听就收藏</span>
+          <div class="movie-button">
+            <div>
+              <a href="https://music.163.com/#/playlist?id=617382486" target="_blank">
+                <span>我的音乐</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="author-content">
+      <div class="author-content-item author-website">
+        <h2>起源</h2>
+        <p><strong>文武科技柜</strong>是 <a href="https://blog.wangdu.site/" target="_blank">文武科技社</a> 的附属产物</p>
+        <p>一开始斌仔在 文武科技社 写一些文章，因为使用的是 Github+Hexo，访问速度很不理想</p>
+        <p>于是就将自己闲置的服务器拿出来搭建了这个 WordPress 博客</p>
+        <p>后来找到了将 <a href="https://www.wangdu.site/software/47.html" target="_blank">WordPress和公众号、小程序三者合一的方法</a></p>
+        <p>于是就一直使用到现在，现在网站运行比较稳定，更新也比较及时</p>
+        <h2>本站资源</h2>
+        <p>本站主要分享一些好用的<strong>工具合集</strong>还有一些根据个人实践经验而编写的<strong>实用教程</strong></p>
+        <p>工具和教程由斌仔亲自整理并发布</p>
+        <p>如果觉得对你有帮助，可以 <code>Ctrl+D</code> 收藏本站哦，防止迷路找不到！</p>
+      </div>
+    </div>
+    <div class="author-content">
+      <div class="author-content-item author-reward">
+        <h2>致谢名单</h2>
+        <p>网站的长久离不开大家的支持和鼓励，有点零碎钱的，尽一份薄利。</p>
+        <p>☕️支持博主：扫描<strong>旗下网站</strong>中的小程序二维码，点击一下广告</p>
+        <p>非常感谢赞赏的朋友，虽然不认识，但是你的小小帮助，对于斌仔来说是莫大的感动❤️</p>
+        <div class="reward-img">
+          <table>
+            <thead>
+              <tr>
+                <th align="center">支付宝</th>
+                <th align="center">微信</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td align="center"><img src="https://img1.wangdu.site/20210330152001.jpg"
+                    data-src="https://img1.wangdu.site/20210330152001.jpg" alt=""></td>
+                <td align="center"><img src="https://img.wangdu.site/2022/202207261118711.png"
+                    data-src="https://img.wangdu.site/2022/202207261118711.png" alt=""></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="reward-lists">
+          <div class="reward-lists-info">
+            <div class="reward-lists-left">
+              <div class="reward-name">**杰</div>
+              <div class="reward-money">￥1.80</div>
+            </div>
+            <div class="reward-lists-right">
+              <div class="reward-type">微 信</div>
+              <div class="reward-time">2021-06-30</div>
+            </div>
+          </div>
+          <div class="reward-lists-info">
+            <div class="reward-lists-left">
+              <div class="reward-name">**杰</div>
+              <div class="reward-money">￥1.80</div>
+            </div>
+            <div class="reward-lists-right">
+              <div class="reward-type">支付宝</div>
+              <div class="reward-time">2021-06-30</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="author-content">
+      <div class="author-content-item author-wechat">
+        <h2>旗下网站</h2>
+        <p>公众号：<strong>文武科技社</strong></p>
+        <p>网站：<a href="https://blog.wangdu.site/" title="文武科技社" target="_blank">文武科技社</a></p>
+        <p><img decoding="async" src="https://tucang.cc/api/image/show/480fc179133eebe12c432236f9ed0819" alt=""></p>
+      </div>
+    </div>
+  </div>
+  
+					</div>
+				</div>
+				</article>
+				<?php comments_template( '', true ); ?>
+				</main>
+			</div>
+		</div>
+	</div>
+</div>
+<?php get_footer();?>
+<style>
+  .about-page {
+    width: 100%;
+    height: 100%;
+  }
+
+  .author-box {
+    width: 10%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 auto;
+  }
+
+  .author-box img {
+    height: 100px;
+    width: 100px;
+    border-radius: 50%;
+  }
+
+  .author-content {
+    width: 100%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .author-content-item {
+    margin-top: 20px;
+    border-radius: 10px;
+    color: #fff;
+    padding: 1rem 2rem;
+  }
+
+  .author-content-left {
+    background: linear-gradient(120deg, #f16b6f 0, #5b27ff 100%);
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    /* border: #00d4ff 1px solid; */
+    position: relative;
+  }
+
+  .item-title2 {
+    font-size: 36px;
+    font-weight: 700;
+    line-height: 1.1;
+    margin: 0.5rem 0;
+  }
+
+
+  .author-content-right {
+    background: linear-gradient(120deg, #5b27ff 0, #00d4ff 100%);
+    width: 40%;
+  }
+
+  .item-link {
+    margin: 0.5rem 0;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    right: 14px;
+  }
+  
+  .item-link a{
+    margin-top: 4px;
+  }
+
+  .item-link img {
+    width: 30px;
+    background-color: #fff;
+    border-radius: 50%;
+  }
+
+  .item-motto {
+    font-size: 40px;
+    font-weight: 700;
+    line-height: 1.1;
+    margin: 0.5rem 0;
+  }
+
+  .author-movies {
+    width: 40%;
+    border-radius: 10px;
+    min-height: 300px;
+    /* color: rgb(255, 244, 219); */
+
+  }
+
+  .item-movies {
+    width: 100%;
+    height: 100%;
+    background: url('https://cdn.wangdu.site/WP/images/bg-movies.jpg') no-repeat top;
+    background-size: cover;
+    overflow: hidden;
+    position: relative;
+    color: #fff;
+    border-radius: 10px;
+  }
+
+  .item-good {
+    background: url('https://cdn.wangdu.site/WP/images/bg-good.jpg') no-repeat;
+    background-size: cover;
+  }
+
+  .item-music {
+    background: url('https://cdn.wangdu.site/WP/images/bg-music.jpg') no-repeat;
+    background-size: cover;
+  }
+
+  .item-movies::before {
+    content: "";
+    background-color: rgba(0, 0, 0, 0.3);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+  }
+
+  .movie-category {
+    position: absolute;
+    top: 1rem;
+    left: 2rem;
+    font-size: 24px;
+    /* text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5); */
+  }
+
+  .item-movies>span {
+    position: absolute;
+    top: 40%;
+    left: 12%;
+    font-size: 40px;
+    font-weight: 700;
+    /* text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5); */
+  }
+
+  .movie-button {
+    position: absolute;
+    bottom: 1rem;
+    right: 2rem;
+    font-size: 20px;
+    font-weight: 700;
+    /* text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5); */
+  }
+
+  .movie-button a {
+    width: 140px;
+    height: 40px;
+    text-decoration: none;
+    background-color: #fff;
+    color: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 20px;
+  }
+
+  .author-website {
+    /* background: linear-gradient(120deg, #f16b6f 0, #5b27ff 100%); */
+    background: linear-gradient(120deg, #00d4ff 0, #5b27ff 100%);
+    width: 100%;
+    letter-spacing: 1px;
+  }
+
+  .author-reward {
+    background: linear-gradient(120deg, #f16b6f 0, #5b27ff 100%);
+    /* background: linear-gradient(120deg, #00d4ff 0, #5b27ff 100%); */
+    width: 100%;
+  }
+
+  .author-reward img {
+    width: 200px;
+  }
+
+  .reward-lists {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .reward-lists-info {
+    width: calc((100% / 6) - 0.5rem);
+    background-color: white;
+    color: black;
+    display: flex;
+    flex-direction: initial;
+    justify-content: space-around;
+    border-radius: 10px;
+    padding: 1rem;
+    margin-right: 5px;
+    margin-top: 5px;
+    font-size: small;
+  }
+
+  .reward-lists-left,
+  .reward-lists-right {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: space-around;
+  }
+
+  .reward-name {
+    align-self: flex-start;
+    font-weight: bold;
+  }
+
+  .reward-money {
+    background-color: #000;
+    color: #fff;
+    padding: 4px;
+    border-radius: 6px;
+  }
+  
+  .author-wechat{
+    background: linear-gradient(120deg, #00d4ff 0, #f16b6f 100%);
+    width: 100%;
+  }
+
+  .author-wechat img{
+    max-width: 100%;
+    border-radius: 10px;
+  }
+  
+  @media screen and (max-width: 1900px) {
+    .author-content {
+      flex-wrap: wrap;
+    }
+
+    .author-content-left,
+    .author-content-right,
+    .author-movies {
+      width: 100%;
+    }
+
+    .reward-lists-info {
+      width: calc((100% / 4) - 0.5rem);
+    }
+
+    .reward-img {
+      display: flex;
+      justify-content: center;
+    }
+  }
+  
+  @media screen and (max-width: 768px) {
+    .reward-lists-info {
+      width: 100%;
+    }
+  }
+</style>
+<script type="text/javascript">
+  function callback(ip, location, asn, org)
+  {
+      var html = `${location}`;
+      document.getElementById('address').innerHTML = html;
+  }
+</script>
+<script type="text/javascript" src="https://ip.ping0.cc/geo/jsonp/callback"></script>
